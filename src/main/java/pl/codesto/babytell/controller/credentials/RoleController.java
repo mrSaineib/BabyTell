19ab7e041626e73ab395d5425399b56432341d5e
Marcin Bienias <marcin@niemainternetu.pl>
2019-02-01 20:14:42 +0100

    RoleController

---
+++ src/main/java/pl/codesto/babytell/controller/credentials/RoleController.java
@@ -0,0 +1,36 @@
+package pl.codesto.babytell.controller.credentials;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.codesto.babytell.service.credentials.RoleDto;
+import pl.codesto.babytell.service.credentials.RoleService;
+
+@Controller
+@RequestMapping("role")
+public class RoleController {
+
+    @Autowired
+    RoleService roleService;
+
+    @RequestMapping(value = "createOrUpdate", method = RequestMethod.POST)
+    public String createOrUpdate(@ModelAttribute RoleDto role) {
+        Long roleId = role.getId();
+        if (roleId == null) {
+            roleService.addRole(role);
+        } else {
+            roleService.updateRole(roleId, role);
+        }
+
+        return "forward:/all";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public String delete(@ModelAttribute RoleDto role) {
+        Long roleId = role.getId();
+        roleService.deleteRole(roleId);
+        return "forward:/all";
+    }
+}
