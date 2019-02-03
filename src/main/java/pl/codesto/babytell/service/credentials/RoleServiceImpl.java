ba252f50d7260af12b92947135a4c04b04e2b77f
mrSaineib <42607417+mrSaineib@users.noreply.github.com>
2019-02-03 18:43:09 +0100

    US-8 Merge: Added basic Role functionality (#2)
    * Role Entity
    
    * Role Repository
    
    * Mappable interface and RoleMapper
    
    * RoleService
    
    * RoleController
    
    * Service fixes, removing spring security

---
+++ src/main/java/pl/codesto/babytell/service/credentials/RoleServiceImpl.java
@@ -0,0 +1,54 @@
+package pl.codesto.babytell.service.credentials;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.codesto.babytell.domain.credentials.Role;
+import pl.codesto.babytell.repository.RoleRepository;
+import pl.codesto.babytell.service.util.Mappable;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    @Qualifier("RoleMapper")
+    Mappable roleMapper;
+
+    @Override
+    public Role addRole(RoleDto roleDto) {
+        Role dbRole = (Role) roleMapper.mapToDb(roleDto);
+        roleRepository.save(dbRole);
+        return dbRole;
+    }
+
+    @Override
+    public Role updateRole(Long roleId, RoleDto roleDto) {
+        Role dbRole = roleRepository.getOne(roleId);
+        dbRole.setName(roleDto.getName());
+        roleRepository.save(dbRole);
+        return dbRole;
+    }
+
+    @Override
+    public boolean deleteRole(Long roleId) {
+        try {
+            roleRepository.deleteById(roleId);
+        } catch (EmptyResultDataAccessException ex) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<Role> allRoles() {
+        return roleRepository.findAll();
+    }
+}
