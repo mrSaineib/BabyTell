34171c84a612b15b15d57bf931b83a44ea3f8160
Marcin Staszek <marcin.staszek88@gmail.com>
2019-02-04 00:00:13 +0100

    User Model

---
+++ src/main/java/pl/codesto/babytell/domain/credentials/User.java
@@ -0,0 +1,13 @@
+package pl.codesto.babytell.domain.credentials;
+
+import pl.codesto.babytell.domain.base.BaseEntity;
+
+public class User extends BaseEntity {
+
+   private String userName;
+   private String password;
+   private String email;
+   private boolean active;
+
+
+}
